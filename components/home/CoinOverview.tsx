5421b9f64c1a5289d2f81af34fccd6f04acec33b
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-01-05 21:55:21 +0100

    the chart is finally implemented, still needs some improvements bu the big boulder is out of the way now

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,29 +1,46 @@
 import { fetcher } from "@/lib/coingecko.actions";
 import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
 import { CoinOverviewFallback } from "./fallback";
+import CandlestickChart from "../CandleStickChart";
 
 const CoinOverview = async () => {
-  let coin;
+  let coin, coinOHLCData;
 
   try {
-    coin = await fetcher<CoinDetailsData>("coins/bitcoin", {
-      dex_pair_format: "symbol",
-    });
+    [coin, coinOHLCData] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        tickers: true,
+      }),
+      fetcher<OHLCData[]>("/coins/bitcoin/ohlc", {
+        vs_currency: "usd",
+        days: 1,
+        precision: "full",
+      }),
+    ]);
   } catch (error) {
-    console.error(`Error fetching coin overview`, error);
+    console.error("Error fetching coin overview:", error);
     return <CoinOverviewFallback />;
   }
+
   return (
     <div id="coin-overview">
-      <div className="header pt-2">
-        <Image src={coin.image.large} alt={coin.name} width={56} height={56} />
-      </div>
-      <div className="info">
-        <p>
-          {coin.name} / {coin.symbol.toLowerCase()}{" "}
-        </p>
-        <h1>${coin.market_data.current_price.usd} </h1>
-      </div>
+      <CandlestickChart data={coinOHLCData} coinId="bitcoin">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
+        </div>
+      </CandlestickChart>
     </div>
   );
 };
