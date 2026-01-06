8d1f52ef90edddb29efff6802a450b162ec47b80
21Kl3v1s21 <klevis25sfj@gmail.com>
2026-01-06 20:03:54 +0100

    categories section has been added

--- components/DataTable.tsx
+++ components/DataTable.tsx
@@ -1,7 +1,6 @@
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -28,7 +27,9 @@ const DataTable = <T,>({
             <TableHead
               key={i}
               className={cn(
-                "bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5"
+                "bg-dark-400 text-purple-100 py-4 first:pl-5 last:pr-5",
+                headerCellClassName,
+                column.headClassName
               )}
             >
               {column.header}
@@ -41,14 +42,18 @@ const DataTable = <T,>({
           <TableRow
             key={rowKey(row, rowIndex)}
             className={cn(
-              "overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative ",
+              "overflow-hidden rounded-lg border-b border-purple-100/5 hover:bg-dark-400/30! relative",
               bodyRowClassName
             )}
           >
-            {columns.map((column, columnIdex) => (
+            {columns.map((column, columnIndex) => (
               <TableCell
-                key={columnIdex}
-                className={cn("py-4 first:pl-5 last:pr-5 ")}
+                key={columnIndex}
+                className={cn(
+                  "py-4 first:pl-5 last:pr-5",
+                  bodyCellClassName,
+                  column.cellClassName
+                )}
               >
                 {column.cell(row, rowIndex)}
               </TableCell>
